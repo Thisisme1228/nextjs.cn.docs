795e75a61a1b014501cb3e5e79004566ef3342cd
Helene <878723827@qq.com>
2024-08-16 13:22:15 +0800

    init

---
+++ docusaurus.config.ts
@@ -0,0 +1,121 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Next.js",
+  tagline: "中文网站",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "14",
+      logo: {
+        alt: "Next.js",
+        src: "img/logo.svg",
+        srcDark: "img/logo_dark.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "中文文档",
+        },
+        {
+          type: "search",
+          position: "right",
+        },
+        {
+          href: "https://nextjs.org/docs",
+          label: "英文文档",
+          position: "right",
+        },
+        {
+          href: "https://vercel.com/new",
+          label: "部署",
+          position: "right",
+        },
+      ],
+    },
+    algolia: {
+      appId: "AUJYIQ70HN",
+      apiKey: "25243dbf9049cf036e87f64b361bd2b9",
+      indexName: "nextjs.cn.docs",
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          label: "中文文档出处：Thisisme1228/nextjs.cn.docs 仓库",
+          position: "center",
+          href: "https://github.com/Thisisme1228/nextjs.cn.docs",
+        },
+      ],
+      copyright: `本站点用于展示 Next.js 14中文文档，所有中文文档由个人翻译. `,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
