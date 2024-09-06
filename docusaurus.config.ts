2391f7c9644822ff4d2f754cbb661a14f6833b5f
Helene <878723827@qq.com>
2024-09-06 09:04:08 +0800

    change shared url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://nextjs.thisisme1228.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
